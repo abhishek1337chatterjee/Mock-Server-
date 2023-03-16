664062bf332e730e52ab71f3353bf257b531e99c
abhishek1337chatterjee <abhishek1337chatterjee@gmail.com>
2023-03-16 12:07:57 +0530

    added the last thing

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 
-server.post('/auth/register', (req, res) => {
-  const db = router.db;
-  const users = db.get('users').value();
-  const userExists = users.find(user => user.email === req.body.email);
-  if (userExists) {
-    res.status(400).send({ error: 'Email already exists' });
-  } else {
-    db.get('users').push(req.body).write();
-    res.status(201).send({ message: 'User created successfully' });
-  }
-});
+// server.post('/auth/register', (req, res) => {
+//   const db = router.db;
+//   const users = db.get('users').value();
+//   const userExists = users.find(user => user.email === req.body.email);
+//   if (userExists) {
+//     res.status(400).send({ error: 'Email already exists' });
+//   } else {
+//     db.get('users').push(req.body).write();
+//     res.status(201).send({ message: 'User created successfully' });
+//   }
+// });
 
 server.post('/auth/login', (req, res) => {
   const db = router.db;
